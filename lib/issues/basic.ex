1d8ee814fb1616a96ef081320c632a1d689e85d8
Vladimir Moravec <vmoravec@suse.com>
2015-10-06 23:45:56 +0200

    Add links

--- lib/issues/basic.ex
+++ lib/issues/basic.ex
@@ -1,8 +1,8 @@
 defmodule Issues.Basic do
-  def greet do
+  def greet whatever do
     receive do
-      { sender, msg } -> send(sender, { :ok, "Hello, #{msg}"})
-      greet
+      { sender, msg } -> send(sender, { :ok, "Hello, #{msg}\n#{whatever}"})
+      greet whatever
     end
   end
 end
@@ -10,11 +10,53 @@ end
 defmodule Issues.Receiver do
   require Logger
 
-  def say do
-    pid = spawn(Issues.Basic, :greet, [])
+  def say something do
+    pid = spawn(Issues.Basic, :greet, [something])
     send(pid, { self, "good morning" })
     receive do
       {:ok, message} -> Logger.info((message)); Logger.info(inspect pid)
     end
   end
 end
+
+defmodule Link do
+  import :timer, only: [ sleep: 1 ]
+
+  def sad do
+    IO.puts "sleeping"
+    sleep 500
+    receive do
+      { :ok, guest } -> send(guest, "THIS IS FROM SAD, #{inspect self}")
+    end
+    exit(:boom)
+  end
+
+  def run do
+    pid = spawn(Link, :sad, [])
+    send(pid, { :ok, self })
+    receive do
+      msg -> IO.puts "Arrived: #{msg}"
+    after 1000 -> IO.puts "after 100 nothing happend"
+    end
+  end
+
+end
+
+defmodule LinkT do
+  import :timer, only: [ sleep: 1 ]
+
+  def sad do
+    sleep 100
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_link(LinkT, :sad, [])
+    receive do
+      msg -> IO.puts "MESSAGE: #{inspect msg}"
+    after 200 -> IO.puts "Nothing happend"
+    end
+  end
+end
+
+LinkT.run
